5fdcb359b5b304491d4f29258f23cfe3ee956f4e
ldureuil <lilas.dureuil@outlook.fr>
2025-01-31 15:37:41 +0100

    Amélioration de la classe graphe (vector -> unordered_map, suppression de la redirection) (non testée)

--- Graphe.cpp
+++ Graphe.cpp
@@ -28,64 +28,46 @@ void Graphe::toDot ( const string nom ) const
 {
     ofstream fichier ( nom );
 
-    // Redirection du stream buffer associé à l’objet cout sur le stream buffer associé
-    // à l’objet fichier en conservant l’adresse de l’ancien stream buffer de cout
-    streambuf *oldCoutBuffer = cout.rdbuf ( fichier.rdbuf( ) );
+    fichier << "digraph {" << endl;
 
-    // Affichage formaté avec cout, redirigé dans notre fichier à cause du changement de stream buffer
-    cout << "digraph {" << endl;
-
-    vector<string> correspondances;
+    unordered_map<string, int> correspondances;
+    int next_index = 0;
+    int indexA, indexB;
+    unordered_map<string, int>::iterator it_A;
+    unordered_map<string, int>::iterator it_B;
 
      // Parcours du graphe pour ajouter ses arrêtes
     for (graphe::iterator it1 = (*graph).begin ( ) ; it1 != (*graph).end ( ) ; ++it1 ) {
         for (unordered_map<string, int>::iterator it2 = it1->second.first.begin ( ) ; it2 != it1->second.first.end ( ) ; ++it2 ) {
             // if A not in correspondances add A and node number, else find node number
-            int indexA;
 
-            vector<string>::iterator it_c = correspondances.begin ( );
-            while ((it_c != correspondances.end ( )) && (*it_c != (*it1).first)) {
-                ++it_c;
-            }
-
-            if (it_c == correspondances.end ( )) {
-                indexA = correspondances.size ( );
-                correspondances.push_back((*it1).first);
+            it_A = correspondances.find ( (*it1).first );
+            it_B = correspondances.find ( (*it2).first );
+ 
+            if (it_A == correspondances.end ( )) {
+                correspondances.insert( pair<string, int>( (*it1).first, next_index ));
+                indexA = next_index++;
+                fichier << "node" << indexA << " [label=\"" << (*it1).first << "\"];" << endl;
             } else {
-                indexA = it_c - correspondances.begin ( );
+                indexA = (*it_A).second;
             }
 
-            cout << "node" << indexA << " [label=\"" << (*it1).first << "\"];" << endl;
-
-            // if B not in correspondances add B and node number, else find node number
-            int indexB;
-
-            it_c = correspondances.begin ( );
-            while ((it_c != correspondances.end ( )) && (*it_c != (*it2).first)) {
-                ++it_c;
-            }
-
-            if (it_c == correspondances.end ( )) {
-                indexB = correspondances.size ( );
-                correspondances.push_back((*it2).first);
+            if (it_B == correspondances.end ( )) {
+                correspondances.insert( pair<string, int>( (*it2).first, next_index ));
+                indexB = next_index++;
+                fichier << "node" << indexB << " [label=\"" << (*it2).first << "\"];" << endl;
             } else {
-                indexB = it_c - correspondances.begin ( );
+                indexB = (*it_B).second;
             }
-
-            cout << "node" << indexB << " [label=\"" << (*it2).first << "\"];" << endl;
 
             // add A -> B
-            cout << "node" << indexA << " -> node" << indexB << " [label=\"" << (*it2).second << "\"];" << endl;
+            fichier << "node" << indexA << " -> node" << indexB << " [label=\"" << (*it2).second << "\"];" << endl;
         }
     }
 
-    cout << "}" << endl;
+    fichier << "}" << endl;
 
-    // Restauration du stream buffer par défaut pour cout, c’est-à-dire la console 
-    cout.rdbuf ( oldCoutBuffer);
-
-    // Affichage formaté à l’aide de l’objet cout, à nouveau sur la console
-    cout << "Dot-file " << nom << " generated" << endl;
+    std::cout << "Dot-file " << nom << " generated" << endl;
 
     // Fermeture du fichier redirection.txt
     fichier.close ( );
